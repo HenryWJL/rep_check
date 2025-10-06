0f209d9d37cd7d7345a263814e8de8be44b38aef
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-06 17:56:58 -0400

    Add curve saving options to plot_curves

--- rep_check/utils/visualizer.py
+++ rep_check/utils/visualizer.py
@@ -6,24 +6,25 @@ def plot_curves(
     num_epochs: int,
     loss: List,
     accuracy: List,
-    train: Optional[bool] = True
+    filename: Optional[str] = None
 ) -> None:
     assert len(loss) == len(accuracy)
     epochs = range(1, num_epochs + 1, num_epochs // len(loss))
-    split = "Training" if train else "Validation"
     plt.figure(figsize=(12, 5))
     # Plot loss
     plt.subplot(1, 2, 1)
     plt.plot(epochs, loss, 'b-')
-    plt.title(f'{split} Loss')
+    plt.title('Loss Curve')
     plt.xlabel('Epoch')
     plt.grid(True, linestyle="--", alpha=0.6)
     # Plot accuracy
     plt.subplot(1, 2, 2)
     plt.plot(epochs, accuracy, 'r-')
-    plt.title(f'{split} Accuracy')
+    plt.title('Accuracy Curve')
     plt.xlabel('Epoch')
     plt.grid(True, linestyle="--", alpha=0.6)
 
     plt.tight_layout()
     plt.show()
+    if filename is not None:
+        plt.savefig(filename, bbox_inches='tight', pad_inches=0.0, dpi=300)
