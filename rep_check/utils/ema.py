0a17cfc0167d2e8c275650545a192cd16c4b61b7
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-10-08 17:50:36 -0400

    Add ema.py

---
+++ rep_check/utils/ema.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules.batchnorm import _BatchNorm
+from typing import Optional
+
+
+# Adapted from https://github.com/real-stanford/diffusion_policy/blob/548a52bbb105518058e27bf34dcf90bf6f73681a/diffusion_policy/model/diffusion/ema_model.py
+class EMA:
+    
+    def __init__(
+        self,
+        model: nn.Module,
+        update_after_step: Optional[int] = 0,
+        inv_gamma: Optional[float] = 1.0,
+        power: Optional[float] = 2/3,
+        min_value: Optional[float] = 0.,
+        max_value: Optional[float] = 0.9999
+    ) -> None:
+        """
+        Exponential Moving Average of models weights
+
+        @crowsonkb's notes on EMA Warmup:
+            If gamma=1 and power=1, implements a simple average. gamma=1, power=2/3 are good values for models you plan
+            to train for a million or more steps (reaches decay factor 0.999 at 31.6K steps, 0.9999 at 1M steps),
+            gamma=1, power=3/4 for models you plan to train for less (reaches decay factor 0.999 at 10K steps, 0.9999
+            at 215.4k steps).
+        Args:
+            inv_gamma (float): Inverse multiplicative factor of EMA warmup. Default: 1.
+            power (float): Exponential factor of EMA warmup. Default: 2/3.
+            min_value (float): The minimum EMA decay rate. Default: 0.
+        """
+        self.averaged_model = model
+        self.averaged_model.eval()
+        self.averaged_model.requires_grad_(False)
+
+        self.update_after_step = update_after_step
+        self.inv_gamma = inv_gamma
+        self.power = power
+        self.min_value = min_value
+        self.max_value = max_value
+
+        self.decay = 0.0
+        self.optimization_step = 0
+
+    def get_decay(self, optimization_step):
+        """
+        Compute the decay factor for the exponential moving average.
+        """
+        step = max(0, optimization_step - self.update_after_step - 1)
+        value = 1 - (1 + step / self.inv_gamma) ** -self.power
+        if step <= 0:
+            return 0.0
+        return max(self.min_value, min(value, self.max_value))
+
+    @torch.no_grad()
+    def step(self, new_model):
+        self.decay = self.get_decay(self.optimization_step)
+        for module, ema_module in zip(new_model.modules(), self.averaged_model.modules()):            
+            for param, ema_param in zip(module.parameters(recurse=False), ema_module.parameters(recurse=False)):
+                # iterative over immediate parameters only.
+                if isinstance(param, dict):
+                    raise RuntimeError('Dict parameter not supported')
+                if isinstance(module, _BatchNorm):
+                    # skip batchnorms
+                    ema_param.copy_(param.to(dtype=ema_param.dtype).data)
+                elif not param.requires_grad:
+                    ema_param.copy_(param.to(dtype=ema_param.dtype).data)
+                else:
+                    ema_param.mul_(self.decay)
+                    ema_param.add_(param.data.to(dtype=ema_param.dtype), alpha=1 - self.decay)
+        self.optimization_step += 1
