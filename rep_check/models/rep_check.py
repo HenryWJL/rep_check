ead27db141b2528d3f3208e9c1c10d50f86c102b
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2025-11-15 21:30:39 -0500

    Add rep_check.py

---
+++ rep_check/models/rep_check.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import mediapipe as mp
+from einops import rearrange
+from rep_check.models.gcn import STGCN
+
+
+class RepCheck(nn.Module):
+
+    def __init__(
+        self,
+        cls_model: STGCN,
+    ) -> None:
+        super().__init__()
+        self.cls_model = cls_model
+        self.mp_pose = mp.solutions.pose.Pose(
+            static_image_mode=False,
+            model_complexity=1,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        batch_size = x.shape[0]
+        device = x.device
+        x = rearrange(x, "b t c h w -> (b t) h w c").cpu().numpy()
+        # Unnormalize to 0-255
+        x = (x * 255).astype(np.uint8)
+        pose_landmarks = []
+        for frame in x:
+            results = self.mp_pose.process(frame)
+            if results.pose_landmarks:
+                pose_landmark = []
+                for lm in results.pose_landmarks.landmark[11:]:
+                    pose_landmark.append(np.array([lm.x, lm.y, lm.z, lm.visibility]))
+                # Pelvis as center
+                left_hip = results.pose_landmarks.landmark[23]
+                right_hip = results.pose_landmarks.landmark[24]
+                pelvis = np.array([
+                    (left_hip.x + right_hip.x) / 2,
+                    (left_hip.y + right_hip.y) / 2,
+                    (left_hip.z + right_hip.z) / 2,
+                    0.0
+                ])
+                pose_landmark.append(pelvis)
+                pose_landmark = np.stack(pose_landmark)
+                # Center coords by subtracting by pelvis coords
+                pose_landmark[:, :3] -= pelvis[:3]
+                pose_landmarks.append(pose_landmark)
+            else:
+                # If no pose detected, fill zeros
+                pose_landmarks.append(np.zeros((23, 4), dtype=np.float32))
+        pose_landmarks = np.stack(pose_landmarks)
+        pose_landmarks = rearrange(pose_landmarks, "(b t) v c -> b c t v", b=batch_size)
+        pose_landmarks = torch.from_numpy(pose_landmarks).float().to(device)
+        print(pose_landmarks.shape)
+        logits = self.cls_model(pose_landmarks)
+        return logits
